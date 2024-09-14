983b0e676c1464ebba29aa655962adbe2f933c07
Anurag singh <144618219+anuragsingh132200@users.noreply.github.com>
2024-09-14 22:58:52 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb+srv://e-learning-platform:6TDygw7JiV6X9_A@cluster0.oizobcd.mongodb.net/newdb?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('YOUR DB URL');
 
 
 const db = mongoose.connection;
